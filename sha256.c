c485a8ec78f2389265b6d8808fd39f62346916ec
BrianD147 <briandoyle147@instance-1.europe-west2-c.c.theory-of-algorithms.internal>
2019-03-29 13:46:57 +0000

    Added nessessary variables

--- sha256.c
+++ sha256.c
@@ -4,7 +4,35 @@
 #include <stdio.h> // Usual input/output header file
 #include <stdin.h> // For using fixed bit length integers
 
+void sha256();
+
 int main (int argc, char *argv[]){
   
   return 0;
+}
+
+void sha256(){
+
+  uint32_t W[64]; // Message schedule (Section 6.2).
+  uint32_t a, b, c, d, e, f, g, h; // Working variables (Section 6.2).
+  uint32_t T1, T2; // Two temporary variables (section 6.2).
+
+  // The Hash value (Section 6.2).
+  // Values come from Section 5.3.3.
+  uint32_t H[8] = {
+    0x6a09e667,
+    0xbb67ae85,
+    0x3c6ef372,
+    0xa54ff53a,
+    0x510e527f,
+    0x9b05688c,
+    0x1f83d9ab,
+    0x5be0cd19
+  }
+
+  // Current message block
+  uint32_t M[16];
+
+
+
 }
