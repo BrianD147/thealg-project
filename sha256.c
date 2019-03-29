24205288a1d0cb26bbddc04cbd5114c53247f3c5
BrianD147 <briandoyle147@gmail.com>
2019-03-29 15:08:48 +0000

    Added Step 3

--- sha256.c
+++ sha256.c
@@ -58,6 +58,19 @@ void sha256(){
   a = H[0]; b = H[1]; c = H[2]; d = H[3];
   e = H[4]; f = H[5]; g = H[6]; h = H[7]; 
 
+  // Step 3.
+  for (t=0; t<64; t++){
+    T1 = h + SIG_1(e) + Ch(e, f, g) + K[t] + W[t];
+    T2 = SIG_0(a) + Maj(a, b, c);
+    h = g;
+    g = f;
+    f = e;
+    e = d + T1;
+    d = c;
+    c = b;
+    b = a;
+    a = T1 + T2;
+  }
 }
 
 // See Section 3.2 for definitions
