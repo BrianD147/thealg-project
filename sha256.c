f3f1248e23a1e970a0c3178ef0a1e30efd23f269
BrianD147 <briandoyle147@instance-1.europe-west2-c.c.theory-of-algorithms.internal>
2019-03-29 13:26:50 +0000

    Added empty main method

---
+++ sha256.c
@@ -0,0 +1,10 @@
+// Brian Doyle - G00330969
+// The Secure Hash Algorithm, 256 bit version
+
+#include <stdio.h> // Usual input/output header file
+#include <stdin.h> // For using fixed bit length integers
+
+int main (int argc, char *argv[]){
+  
+  return 0;
+}
