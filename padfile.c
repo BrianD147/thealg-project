bfc1357124ca7cd2380096e8cd6fcd4d5f6dc66a
BrianD147 <briandoyle147@gmail.com>
2019-03-30 17:24:39 +0000

    Added msgblock union and fixed typo

--- padfile.c
+++ padfile.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <stdint.h>
 
+union msgblock {
+  uint8_t e[64];
+  uint32_t t[16];
+  uint64_t s[8];
+};
+
 int main(int argc, char *argv[]) {
   
   FILE* f;
   char c;
 
   f = fopen(argv[1], "r");
-  printf("&c\n", fread(&c, 1, 1, f));
+  printf("%c\n", fread(&c, 1, 1, f));
 
   fclose(f);
         
